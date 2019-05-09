a1fa933cbb3ada478d158642545d237857bfe8ab
Hirokazu Miyaji <hirokazu.miyaji@gmail.com>
2019-05-09 18:29:16 +0900

    redis cluster

---
+++ redis-cluster/4.0.10/start.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+PORTS="6379 6380 6381 6382 6383 6384"
+for PORT in ${PORTS}; do
+  redis-server --port ${PORT} --cluster-enabled yes --cluster-config-file nodes.${PORT}.conf --cluster-node-timeout 2000 --daemonize yes
+done
+
+RUNNING="0"
+while [ "${RUNNING}" = "0" ]; do
+    for PORT in ${PORTS}; do
+        redis-cli -p ${PORT} ping > /dev/null
+        if [ $? = 0 ]; then
+            RUNNING="1"
+        else
+            RUNNING="0"
+        fi
+    done
+done
+
+echo "all redis server running..."
+
+HOSTS=""
+for PORT in ${PORTS}; do
+  HOSTS="${HOSTS} 127.0.0.1:${PORT}"
+done
+
+yes "yes" | ruby /redis-trib.rb create --replicas 1 ${HOSTS}
+
+echo "starting redis cluster"
