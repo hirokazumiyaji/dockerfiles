10e3f2a80ef707a5890308ba691b488d9a218ef5
Hirokazu Miyaji <hirokazu.miyaji@gmail.com>
2019-08-01 23:23:05 +0900

    update

--- redis-cluster/4.0.10/docker-entrypoint.sh
+++ redis-cluster/4.0.10/docker-entrypoint.sh
@@ -1,8 +1,29 @@
 #!/bin/sh
 
+CLUSTER_ANNOUNCE_IP=$(/sbin/ip route|awk '/default/ { print $3 }')
+CONTAINER_ID=$(cat /etc/hosts | grep `/sbin/ip route | awk '/src/ { print $7 }'` | awk '{ print $2 }')
+
 PORTS="6379 6380 6381 6382 6383 6384"
 for PORT in ${PORTS}; do
-  redis-server --port ${PORT} --cluster-enabled yes --cluster-config-file nodes.${PORT}.conf --cluster-node-timeout 2000 --daemonize yes
+  CPORT=""
+  case "${PORT}" in
+    "6379" ) CPORT="16379" ;;
+    "6380" ) CPORT="16380" ;;
+    "6381" ) CPORT="16381" ;;
+    "6382" ) CPORT="16382" ;;
+    "6383" ) CPORT="16383" ;;
+    "6384" ) CPORT="16384" ;;
+  esac
+  EXPOSE_PORT=$(curl --unix-socket /var/run/docker.sock http://localhost/containers/a872585638c5/json  | jq .NetworkSettings.Ports | jq '.["${PORT}/tcp"]'[0].HostPort | jq tonumber)
+  EXPOSE_CPORT=$(curl --unix-socket /var/run/docker.sock http://localhost/containers/a872585638c5/json  | jq .NetworkSettings.Ports | jq '.["${CPORT}/tcp"]'[0].HostPort | jq tonumber)
+  redis-server --port ${PORT} \
+               --cluster-enabled yes \
+               --cluster-config-file nodes.${PORT}.conf \
+               --cluster-node-timeout 2000 \
+               --cluster-announce-ip ${CLUSTER_ANNOUNCE_IP} \
+               --cluster-announce-port ${EXPOSE_PORT} \
+               --cluster-announce-bus-port ${EXPOSE_CPORT} \
+               --daemonize yes
 done
 
 RUNNING="0"
