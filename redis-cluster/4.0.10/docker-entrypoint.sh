2beaf8179cd770bc162584ef5cee7315a373356f
Hirokazu Miyaji <hirokazu.miyaji@gmail.com>
2019-08-05 16:13:17 +0900

    fix redis cluster container

--- redis-cluster/4.0.10/docker-entrypoint.sh
+++ redis-cluster/4.0.10/docker-entrypoint.sh
@@ -1,42 +1,20 @@
 #!/bin/sh
 
-CLUSTER_ANNOUNCE_IP=$(/sbin/ip route|awk '/default/ { print $3 }')
-CONTAINER_ID=$(cat /etc/hosts | grep `/sbin/ip route | awk '/src/ { print $7 }'` | awk '{ print $2 }')
+REDIS_CLUSTER_PORTS=${REDIS_CLUSTER_PORTS:-"6379 6380 6381 6382 6383 6384"}
+echo "cluster port = ${REDIS_CLUSTER_PORTS}"
 
-PORTS="6379 6380 6381 6382 6383 6384"
-for PORT in ${PORTS}; do
-    CPORT=""
-    case "${PORT}" in
-        "6379" ) CPORT="16379" ;;
-        "6380" ) CPORT="16380" ;;
-        "6381" ) CPORT="16381" ;;
-        "6382" ) CPORT="16382" ;;
-        "6383" ) CPORT="16383" ;;
-        "6384" ) CPORT="16384" ;;
-    esac
-    if [ -e /var/run/docker.sock ]; then
-        EXPOSE_PORT=$(curl --unix-socket /var/run/docker.sock http://localhost/containers/${CONTAINER_ID}/json  | jq .NetworkSettings.Ports | jq ".[\"${PORT}/tcp\"]"[0].HostPort | jq tonumber)
-        EXPOSE_CPORT=$(curl --unix-socket /var/run/docker.sock http://localhost/containers/${CONTAINER_ID}/json  | jq .NetworkSettings.Ports | jq ".[\"${CPORT}/tcp\"]"[0].HostPort | jq tonumber)
-        redis-server --port ${PORT} \
-                     --cluster-enabled yes \
-                     --cluster-config-file nodes.${PORT}.conf \
-                     --cluster-node-timeout 2000 \
-                     --cluster-announce-ip ${CLUSTER_ANNOUNCE_IP} \
-                     --cluster-announce-port ${EXPOSE_PORT} \
-                     --cluster-announce-bus-port ${EXPOSE_CPORT} \
-                     --daemonize yes
-    else
-        redis-server --port ${PORT} \
-                     --cluster-enabled yes \
-                     --cluster-config-file nodes.${PORT}.conf \
-                     --cluster-node-timeout 2000 \
-                     --daemonize yes
-    fi
+for PORT in ${REDIS_CLUSTER_PORTS}; do
+    redis-server --bind 0.0.0.0 \
+                 --port ${PORT} \
+                 --cluster-enabled yes \
+                 --cluster-config-file nodes.${PORT}.conf \
+                 --cluster-node-timeout 2000 \
+                 --daemonize yes
 done
 
 RUNNING="0"
 while [ "${RUNNING}" = "0" ]; do
-    for PORT in ${PORTS}; do
+    for PORT in ${REDIS_CLUSTER_PORTS}; do
         redis-cli -p ${PORT} ping > /dev/null
         if [ $? = 0 ]; then
             RUNNING="1"
@@ -46,15 +24,15 @@ while [ "${RUNNING}" = "0" ]; do
     done
 done
 
-echo "all redis server running..."
-
 HOSTS=""
-for PORT in ${PORTS}; do
+for PORT in ${REDIS_CLUSTER_PORTS}; do
     HOSTS="${HOSTS} 127.0.0.1:${PORT}"
 done
 
+echo "hosts = ${HOSTS}"
+
 yes "yes" | ruby /usr/local/bin/redis-trib.rb create --replicas 1 ${HOSTS}
 
-echo "starting redis cluster"
+echo "Redis cluster is already accepted connections"
 
 exec "$@"
